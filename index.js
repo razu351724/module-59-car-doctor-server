10122b0c1cf151a39fbd9def43b446ca97f191fb
RAZU-sheikh <razu351724@gmail.com>
2023-11-01 18:28:56 +0600

    Database and Load services data

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const port = process.env.PORT || 5100;
 app.use(cors());
 app.use(express.json());
 
-// console.log()
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q1yeyxk.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,12 +24,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client.db('module-59-carDoctor').collection('services');
+
+app.get('/services', async(req, res) => {
+  const cursor = serviceCollection.find();
+  const result = await cursor.toArray();
+  res.send(result);
+})
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
