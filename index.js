b611bb8b67a53c37b99703716368da6807ee41b9
RAZU-sheikh <razu351724@gmail.com>
2023-11-03 00:30:44 +0600

    Delete API and  Delete a service Booking

--- index.js
+++ index.js
@@ -68,6 +68,16 @@ async function run() {
       res.send(result);
     })
 
+   
+
+    // বুকিং ডিলিট অপারেশন 
+    app.delete('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
