f29817a179de09d00ded26b2ce997b2d758c7ebe
RAZU-sheikh <razu351724@gmail.com>
2023-11-03 01:16:34 +0600

    Update Booking using Patch || mongodb CRUD Opration

--- index.js
+++ index.js
@@ -68,7 +68,19 @@ async function run() {
       res.send(result);
     })
 
-   
+    app.patch('/bookings/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedBooking = req.body;
+      console.log(updatedBooking);
+      const updateDoc = {
+        $set: {
+          status: updatedBooking.status
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
 
     // বুকিং ডিলিট অপারেশন 
     app.delete('/bookings/:id', async (req, res) => {
